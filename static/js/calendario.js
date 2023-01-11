ef548a5a4cb8f7930ee2dad38d6c56a4ccd66f8a
Your Name <you@example.com>
2023-01-11 20:27:05 -0300

    interfaces do projeto final

---
+++ static/js/calendario.js
@@ -0,0 +1,24 @@
+dia = new Array(
+        "domingo",
+        "segunda",
+        "terça",
+        "quarta",
+        "quinta",
+        "sexta",
+        "sábado"
+      );
+      mes = new Array(
+        "janeiro",
+        "fevereiro",
+        "março",
+        "abril",
+        "maio",
+        "junho",
+        "julho",
+        "agosto",
+        "setembro",
+        "outubro",
+        "novembro",
+        "dezembro"
+      );
+      novo = new Date();
